4b2db97dcdf5da9f1183ebff9120f38fadd12da9
Chiara DeLucia <chiaradelucia@Chiaras-MacBook-Pro.local>
2017-02-25 14:15:39 -0500

    Adding logic to push user input data to appropriate files.

--- route.js
+++ route.js
@@ -48,27 +48,40 @@ app.post('/api/table', function (req,res){
 
 		console.log(typeof data); //data is a string (useful info to know so we know how to deal with data)
 
+
 		//parse the string data so it can be pushed into the same array every time
 		var temp = JSON.parse(data);
-		//console.log(temp);
+		console.log(temp);
+		console.log(temp.length);
 
-		//then take the parsed data and push it in to the new customer information (this is in memory)
-		temp.push(customerNew);
-		//console.log(temp);
+		if (temp.length < 5) {
 
-		//take the new information (that is being store in memory) and write it to the file
-		fs.writeFile('./table.txt', JSON.stringify(temp), function (err) {
-			if (err) throw err;
-			console.log("this data was appended");
-			console.log(temp);
-		});
-		
+			//then take the parsed data and push it in to the new customer information (this is in memory)
+			temp.push(customerNew);
+
+			//take the new information (that is being store in memory) and write it to the file
+			fs.writeFile('./table.txt', JSON.stringify(temp), function (err) {
+				if (err) throw err;
+				console.log("this data was appended");
+				console.log(temp);
+			});
+
+		}else{
+			fs.readFile('./waitlist.txt', 'utf8', function(err, data){
+				if (err) throw err;
+				var waitTemp = JSON.parse(data);
+
+				waitTemp.push(customerNew);
+
+				fs.writeFile('./waitlist.txt', JSON.stringify(waitTemp), function (err) {
+					if (err) throw err;
+					console.log("this data was appended");
+					console.log(waitTemp);
+				});
+			});
+		}
 
 	});
-
-
-
-
 
 });
 
