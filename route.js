090a190354f9443603689c2ff3d32fce96d8fb22
Chiara DeLucia <chiaradelucia@Chiaras-MacBook-Pro.local>
2017-02-25 13:53:29 -0500

    Adding logic for POSTing data to txt files.

--- route.js
+++ route.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
+// const table = require('/table.json');
 
 // Sets up the Express App
 // =============================================================
@@ -32,27 +33,42 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Routes
 // =============================================================
-	var hello = [{
-  routeName: "darthmaul",
-  name: "Darth Maul",
+// This is representative of a user inputting their information as an object
+var customerNew = {
+  routeName: "yoda",
+  name: "yoda",
   role: "Sith Lord",
   age: 200,
   forcePoints: 1200
-}]
+}
 
 app.post('/api/table', function (req,res){
-	fs.readFile('table.json', function(err, data){
+	fs.readFile('./table.txt', 'utf8', function(err, data){
 		if (err) throw err;
-	
-		fs.writeFile('table.json', JSON.push(hello), function (err) {
+
+		console.log(typeof data); //data is a string (useful info to know so we know how to deal with data)
+
+		//parse the string data so it can be pushed into the same array every time
+		var temp = JSON.parse(data);
+		//console.log(temp);
+
+		//then take the parsed data and push it in to the new customer information (this is in memory)
+		temp.push(customerNew);
+		//console.log(temp);
+
+		//take the new information (that is being store in memory) and write it to the file
+		fs.writeFile('./table.txt', JSON.stringify(temp), function (err) {
 			if (err) throw err;
-			// console.log("this data was appended");
-// console.log(JSON.parse(data));
+			console.log("this data was appended");
+			console.log(temp);
 		});
 		
 
-
 	});
+
+
+
+
 
 });
 
