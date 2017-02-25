c2a7a61d4353c11b92bc7d8be119edc9db2e1364
Chiara DeLucia <chiaradelucia@Chiaras-MBP.fios-router.home>
2017-02-25 17:38:50 -0500

    Addig GET routes.

--- route.js
+++ route.js
@@ -16,23 +16,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(__dirname));
 
 
 //Reservations (DATA)
 // =============================================================
-// const tables = [];
-// const waitList = [];
 
-// app.get('/api/table' function (req, res){
-// 	console.log('hello');	
-
-// })
-
-
-
-
-// Routes
-// =============================================================
 // This is representative of a user inputting their information as an object
 var customerNew = {
   routeName: "yoda",
@@ -42,6 +31,28 @@ var customerNew = {
   forcePoints: 1200
 }
 
+
+
+
+// Routes
+// =============================================================
+app.get('/', function (req, res){
+	res.sendFile(path.join(__dirname, 'main.html'));
+	res.sendFile(path.join(__dirname, 'main.css'));
+});
+
+app.get('/reserve', function(req, res) {
+	res.sendFile(path.join(__dirname, 'reservations.html'));
+});
+
+
+app.get('/tables', function (req, res){
+	res.sendFile(path.join(__dirname, 'tables.html'));	
+
+});
+
+
+
 app.post('/api/table', function (req,res){
 	fs.readFile('./table.txt', 'utf8', function(err, data){
 		if (err) throw err;
@@ -49,7 +60,8 @@ app.post('/api/table', function (req,res){
 		console.log(typeof data); //data is a string (useful info to know so we know how to deal with data)
 
 
-		//parse the string data so it can be pushed into the same array every time
+		//parse the string data and set it to a temp
+		//var so it can be pushed into the same array every time
 		var temp = JSON.parse(data);
 		console.log(temp);
 		console.log(temp.length);
@@ -67,6 +79,7 @@ app.post('/api/table', function (req,res){
 			});
 
 		}else{
+
 			fs.readFile('./waitlist.txt', 'utf8', function(err, data){
 				if (err) throw err;
 				var waitTemp = JSON.parse(data);
